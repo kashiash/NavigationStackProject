2b6241b5baf54a1dab1185e9edc6ea78df721faa
Jacek Kosinski <kashiash@gmail.com>
2023-03-30 23:46:28 +0200

    Initial Commit

---
+++ NavigationStackProject/NavigationStackProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackProjectApp.swift
+//  NavigationStackProject
+//
+//  Created by Jacek Kosiński G on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
